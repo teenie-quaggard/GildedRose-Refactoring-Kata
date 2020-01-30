f39e3c9807152d810977810f7379ec5424eb9cd0
Kristina Jaggard <kjaggard@8thlight.com>
2020-01-30 10:58:30 +0000

    validation supporting tests for item parameter types

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -10,55 +10,85 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+
+
+  itemValidator(item){
+
+    if (typeof item.name !== 'string') {
+      throw 'Item Name parameter should be a string'
     }
 
-    return this.items;
+    if (typeof item.sellIn !== 'number'){
+      throw 'Item SellIn parameter should be a number'
+    }
+
+    if (typeof item.quality !== 'number' || item.quality < 0 || item.quality > 50){
+      throw 'Item Quality parameter should be a whole number between 0 and 50'
+    }
+
   }
+
+  
+
+  updateQuality(){
+    this.items.map(item => {
+      console.log(item)
+      return this.itemValidator(item);
+    })
+  }
+
+
+
+
+  // updateQuality() {
+  //   for (let i = 0; i < this.items.length; i++) {
+  //     if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+  //       if (this.items[i].quality > 0) {
+  //         if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+  //           this.items[i].quality = this.items[i].quality - 1;
+  //         }
+  //       }
+  //     } else {
+  //       if (this.items[i].quality < 50) {
+  //         this.items[i].quality = this.items[i].quality + 1;
+  //         if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+  //           if (this.items[i].sellIn < 11) {
+  //             if (this.items[i].quality < 50) {
+  //               this.items[i].quality = this.items[i].quality + 1;
+  //             }
+  //           }
+  //           if (this.items[i].sellIn < 6) {
+  //             if (this.items[i].quality < 50) {
+  //               this.items[i].quality = this.items[i].quality + 1;
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+  //       this.items[i].sellIn = this.items[i].sellIn - 1;
+  //     }
+  //     if (this.items[i].sellIn < 0) {
+  //       if (this.items[i].name != 'Aged Brie') {
+  //         if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+  //           if (this.items[i].quality > 0) {
+  //             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+  //               this.items[i].quality = this.items[i].quality - 1;
+  //             }
+  //           }
+  //         } else {
+  //           this.items[i].quality = this.items[i].quality - this.items[i].quality;
+  //         }
+  //       } else {
+  //         if (this.items[i].quality < 50) {
+  //           this.items[i].quality = this.items[i].quality + 1;
+  //         }
+  //       }
+  //     }
+  //   }
+
+  //   return this.items;
+  // }
 }
 
 module.exports = {
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,63 +1,99 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("initialise", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
-  });
-});
+// describe('initialise', () => {
+//   it('should foo', () => {
+//     const gildedRose = new Shop([new Item("foo", 0, 0)]);
+//     const items = gildedRose.updateQuality();
+//     console.log(items, "ITEMSS")
+//     expect(items[0].name).toBe("foo");
+//   });
+// });
 
-describe('special items in store', () => {
-  it('Aged Brie increases in Quality the older it gets', () => {
+// describe('special items in store', () => {
+//   it('Aged Brie increases in Quality the older it gets', () => {
 
+//   })
+
+//   it('Aged Brie increases by 2 when there are 6-10 days left on its SellIn value', () => {
+
+//   })
+
+//   it('Aged Brie increases by 3 when there are 1-5 days left on its SellIn value', () => {
+
+//   })
+
+//   it('Aged Brie quality drops to zero when SellIn value is 0', () => {
+
+//   })
+
+//   it('conjured items degrade twice as fast as normal items', () => {
+
+//   })
+
+//   it('Sulfuras has a quality of 80 and does not degrade', () => {
+
+//   })
+
+// })
+
+describe('general rules for items in store', () => {
+
+  it('should have a Name value that is a string', () => {
+    const gildedRose = new Shop([new Item(true, 0, 0)]);
+    const updateItemQuality = () => {
+      gildedRose.updateQuality();
+    }
+    expect(updateItemQuality).toThrowError('Item Name parameter should be a string');
   })
 
-  it('Aged Brie increases by 2 when there are 6-10 days left on its SellIn value', () => {
-
-  })
-
-  it('Aged Brie increases by 3 when there are 1-5 days left on its SellIn value', () => {
-
-  })
-
-  it('Aged Brie quality drops to zero when SellIn value is 0', () => {
-
-  })
-
-  it('conjured items degrade twice as fast as normal items', () => {
-
-  })
-
-  it('Sulfuras has a quality of 80 and does not degrade', () => {
-
-  })
-
-})
-
-describe('general rules of degregation to most items in store', () => {
   it('should have a SellIn value that is a number', () => {
-
+    const gildedRose = new Shop([new Item('foo', null, 0)]);
+    const updateItemQuality = () => {
+      gildedRose.updateQuality();
+    }
+    expect(updateItemQuality).toThrowError('Item SellIn parameter should be a number');
   })
 
-  it('should have a Quality value that is a positive number', () => {
-    
+  it('should have a Quality value that is a number', () => {
+    const gildedRose = new Shop([new Item('foo', 0, '')]);
+    const updateItemQuality = () => {
+      gildedRose.updateQuality();
+    }
+    expect(updateItemQuality).toThrowError('Item Quality parameter should be a whole number between 0 and 50');
   })
 
-  it('should lower its Sellin value when the updateQuality function is run', () => {
-
+  it('should have a Quality value that is larger than 0', () => {
+    const gildedRose = new Shop([new Item('foo', 0, -1)]);
+    const updateItemQuality = () => {
+      gildedRose.updateQuality();
+    }
+    expect(updateItemQuality).toThrowError('Item Quality parameter should be a whole number between 0 and 50');
   })
 
-  it('should lower its Quality value when the updateQuality function is run', () => {
-
+  it('quality should never be more than 50', () => {
+    const gildedRose = new Shop([new Item('foo', 0, 51)]);
+    const updateItemQuality = () => {
+      gildedRose.updateQuality();
+    }
+    expect(updateItemQuality).toThrowError('Item Quality parameter should be a whole number between 0 and 50');
   })
 
-  it('quality should degrade twice as fast after sell by date', () => {
+  // it('should lower its Sellin value when the updateQuality function is run', () => {
+  //   const initialItem = new Item('foo', 5, 10)
+  //   const gildedRose = new Shop([{...initialItem}]);
+  //   const items = gildedRose.updateQuality();
+  
+  //   expect(items[0].sellIn).toBeLessThan(initialItem);
 
-  })
+  // })
 
-  it('quality should never be negative and never more than 50', () => {
+  // it('should lower its Quality value when the updateQuality function is run', () => {
 
-  })
+  // })
+
+  // it('quality should degrade twice as fast after sell by date', () => {
+
+  // })
+
 
 })
